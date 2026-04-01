fb668549202984687c414d8888b317b8cb25c526
Husey_sz11@itstep.edu.az <Husey_sz11@itstep.edu.az>
2026-04-01 22:35:04 +0400

    start picture upload

---
+++ the_alkanz.Website/Models/ProductAttachment.cs
@@ -0,0 +1,17 @@
+﻿namespace the_alkanz.Website.Models;
+
+public class ProductAttachment
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+
+    public string OriginalFileName {  get; set; } = string.Empty;
+    public string StoredFileName {  get; set; } = string.Empty;
+    public string ContentType {  get; set; } = string.Empty;
+    public long Size {  get; set; } 
+
+    public string UploadedUserId {  get; set; } = string.Empty;
+    public ApplicationUser UploadedUser {  get; set; } = null!;
+    public DateTimeOffset UploadedAt{  get; set; } 
+}
