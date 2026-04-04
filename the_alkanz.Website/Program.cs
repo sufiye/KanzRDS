eb114a8c7fe7fea9b4c094e186e9dba8054e952b
Husey_sz11@itstep.edu.az <Husey_sz11@itstep.edu.az>
2026-04-04 11:35:59 +0400

    all

--- the_alkanz.Website/Program.cs
+++ the_alkanz.Website/Program.cs
@@ -20,6 +20,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+
+{
+
+    options.AddPolicy("AllowFrontend", policy =>
+
+    {
+
+        policy.WithOrigins("http://localhost:5173")
+
+              .AllowAnyHeader()
+
+              .AllowAnyMethod();
+
+    });
+
+});
+
+
 builder.Services.AddSwaggerGen(
      option =>
      {
